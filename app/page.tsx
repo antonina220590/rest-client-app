9e9f387555b72a3865804cdeffae0fc3e0f9a855
Halina Bahdanava <gaaalo44ka@gmail.com>
2025-03-27 22:35:17 -0700

    feat: add custom fonts and colors for styling (#14)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="text-center">
-      <span className="text-blue-700">Home Page</span>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <div>Regular Tex</div>
+      <div>
+        <code>{`
+const multiply = (a, b) => {
+  return a * b;
+};
+`}</code>
+      </div>
+      <Link
+        href="https://www.figma.com/design/sZkORayf0kHQ4kSUBMgdAu/Landing-Page-UI-Kit---Fully-customizable-landing-page-UI-kit---Export-as-HTML-(Community)?node-id=300-7954&t=dwGY7RlMeMcmfqcl-0"
+        className="text-cta-primary text-sm"
+      >
+        Call to action
+      </Link>
     </div>
   );
 }
