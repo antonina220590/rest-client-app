8b471a5c3dc3cb5611ef53a6f32a21da155cb7ba
gbogdanova <gaaalo44ka@gmail.com>
2025-03-30 10:40:42 -0700

    feat: add fairbase with sign in/up logic

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,60 @@
-export default function SignIn() {
+'use client';
+import { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/app/firebase/config';
+import { useRouter } from 'next/navigation';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    try {
+      const res = await signInWithEmailAndPassword(email, password);
+      //console.log({ res });
+      if (!res || !res.user) {
+        throw new Error('Failed to sign in');
+      }
+      sessionStorage.setItem('user', 'true');
+      setEmail('');
+      setPassword('');
+      router.push('/');
+    } catch (error) {
+      throw new Error(
+        `Something happened: ${error instanceof Error ? error.message : 'Unknown error'}`
+      );
+    }
+  };
+
   return (
-    <div className="text-center">
-      <h1>Sign In page</h1>
-    </div>
+    <main className="min-h-[calc(100vh-140px)] flex items-center justify-center">
+      <div className="bg-accent p-10 rounded-lg shadow-xl w-96">
+        <h3 className=" mb-5">Sign In</h3>
+        <input
+          type="email"
+          placeholder="Email*"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-3 mb-4 bg-white rounded outline-none placeholder-cta-secondary"
+        />
+        <input
+          type="password"
+          placeholder="Password*"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-3 mb-4 bg-white rounded outline-none placeholder-cta-secondary"
+        />
+        <button
+          onClick={handleSignIn}
+          className="w-full p-3 bg-cta-primary rounded text-white hover:bg-cta-hover hover:cursor-pointer"
+        >
+          Sign In
+        </button>
+      </div>
+    </main>
   );
-}
+};
+
+export default SignIn;
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,58 @@
-export default function SignUp() {
+'use client';
+import { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from '@/app/firebase/config';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [createUserWithEmailAndPassword] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleSignUp = async () => {
+    try {
+      const res = await createUserWithEmailAndPassword(email, password);
+      //console.log({ res });
+      if (!res || !res.user) {
+        throw new Error('Failed to sign up');
+      }
+      sessionStorage.setItem('user', 'true');
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      throw new Error(
+        `Something happened: ${error instanceof Error ? error.message : 'Unknown error'}`
+      );
+    }
+  };
+
   return (
-    <div className="text-center">
-      <h1>Sign Up page</h1>
-    </div>
+    <main className="min-h-[calc(100vh-140px)] flex items-center justify-center">
+      <div className="bg-accent p-10 rounded-lg shadow-xl w-96">
+        <h3 className=" mb-5">Sign Up</h3>
+        <input
+          type="email"
+          placeholder="Email*"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-3 mb-4 bg-white rounded outline-none placeholder-cta-secondary"
+        />
+        <input
+          type="password"
+          placeholder="Password*"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-3 mb-4 bg-white rounded outline-none placeholder-cta-secondary"
+        />
+        <button
+          onClick={handleSignUp}
+          className="w-full p-3 bg-cta-primary rounded text-white hover:bg-cta-hover hover:cursor-pointer"
+        >
+          Sign Up
+        </button>
+      </div>
+    </main>
   );
-}
+};
+
+export default SignUp;
