b5a1e7db768cbf5fc85a9deb16c6df6c82cd2463
gbogdanova <gaaalo44ka@gmail.com>
2025-03-31 14:58:18 -0700

    fix: add form validation, error handling, and cookies for sign-up

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,55 +1,89 @@
 'use client';
+
 import { useState } from 'react';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from '@/app/firebase/config';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { formSchema } from '@/app/schema/yupShema';
+import Tost from '../components/authentification/tost/Tost';
+import { setCookie } from 'cookies-next';
+
+type FormSignUp = {
+  email: string;
+  password: string;
+};
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [createUserWithEmailAndPassword] =
-    useCreateUserWithEmailAndPassword(auth);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSignUp>({
+    resolver: yupResolver(formSchema),
+    mode: 'onChange',
+  });
 
-  const handleSignUp = async () => {
+  const [createUserWithEmailAndPassword, loading] =
+    useCreateUserWithEmailAndPassword(auth);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const onSubmit = async (data: FormSignUp) => {
     try {
-      const res = await createUserWithEmailAndPassword(email, password);
-      //console.log({ res });
-      if (!res || !res.user) {
-        throw new Error('Failed to sign up');
-      }
-      sessionStorage.setItem('user', 'true');
-      setEmail('');
-      setPassword('');
-    } catch (error) {
-      throw new Error(
-        `Something happened: ${error instanceof Error ? error.message : 'Unknown error'}`
+      const res = await createUserWithEmailAndPassword(
+        data.email,
+        data.password
       );
+      if (!res || !res.user) {
+        setError('Failed to sign up');
+        setTimeout(() => setError(null), 5000);
+        return;
+      }
+      setCookie('user', 'true', { path: '/' });
+      router.push('/');
+    } catch (err) {
+      if (err instanceof Error) {
+        setError('An unknown error occurred during sign-up');
+      }
+      setTimeout(() => setError(null), 5000);
     }
   };
 
   return (
     <main className="min-h-[calc(100vh-140px)] flex items-center justify-center">
+      {error && <Tost error={error} />}
       <div className="bg-accent p-10 rounded-lg shadow-xl w-96">
-        <h3 className=" mb-5">Sign Up</h3>
-        <input
-          type="email"
-          placeholder="Email*"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-3 mb-4 bg-white rounded outline-none placeholder-cta-secondary"
-        />
-        <input
-          type="password"
-          placeholder="Password*"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-3 mb-4 bg-white rounded outline-none placeholder-cta-secondary"
-        />
-        <button
-          onClick={handleSignUp}
-          className="w-full p-3 bg-cta-primary rounded text-white hover:bg-cta-hover transition hover:cursor-pointer"
-        >
-          Sign Up
-        </button>
+        <h3 className="mb-5 text-center">Sign Up</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            placeholder="Email*"
+            className="w-full p-3 mb-2 bg-white rounded outline-none placeholder-cta-secondary"
+            {...register('email')}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm mb-3">{errors.email.message}</p>
+          )}
+          <input
+            type="password"
+            placeholder="Password*"
+            className="w-full p-3 mb-2 bg-white rounded outline-none placeholder-cta-secondary"
+            {...register('password')}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm mb-3">
+              {errors.password.message}
+            </p>
+          )}
+          <button
+            type="submit"
+            className="w-full p-3 bg-cta-primary rounded text-white hover:bg-cta-hover transition hover:cursor-pointer"
+          >
+            {loading ? 'Signing up...' : 'Submit'}
+          </button>
+        </form>
       </div>
     </main>
   );
