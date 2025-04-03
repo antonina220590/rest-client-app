b93f9299b6e8d172a139a020aea6d41d704c10c9
gbogdanova <gaaalo44ka@gmail.com>
2025-04-03 09:25:27 -0700

    feat: sticky animated header

--- app/components/main-route/Header/Header.tsx
+++ app/components/main-route/Header/Header.tsx
@@ -7,16 +7,27 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/app/firebase/config';
 import { deleteCookie, getCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   const [user, loading] = useAuthState(auth);
   const userSession = getCookie('user');
   const router = useRouter();
+  const [scroll, setScroll] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => setScroll(window.scrollY > 50);
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   if (loading) return null;
 
   return (
-    <header>
+    <header
+      className={`transition-all duration-300 ${scroll ? 'opacity-50 py-1' : 'opacity-100 py-4'}`}
+    >
       <h1>
         <Link href="/" className="animate-text-change">
           {'{REST}'}
