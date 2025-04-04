648ce917e2aed55084154b3c83c953c55ab93673
Halina Bahdanava <gaaalo44ka@gmail.com>
2025-04-04 01:37:22 -0700

    feat: general requirements (multiple languages support and sticky header) (#17)
    * feat: sticky animated header
    
    * fix: sticky instead of fixed
    
    * feat: add internationalization (i18n) support for English and Russian
    
    * fix: translate Welcom section
    
    * fix: update setupTests file
    
    * fix: change font for cyrillic support
    
    * feat: translate AboutRSS section
    
    * feat: tests for AboutRSS and Welcome
    
    * feat: translate AboutUs section
    
    * feat: translate sign in/up pages and shema

--- app/components/main-route/Header/Header.tsx
+++ app/components/main-route/Header/Header.tsx
@@ -7,26 +7,43 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/app/firebase/config';
 import { deleteCookie, getCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { useTranslations } from 'next-intl';
+import LanguageSwitcher from './LgSwitcher/LgSwitcher';
 
 export default function Header() {
+  const t = useTranslations('Header');
+
   const [user, loading] = useAuthState(auth);
   const userSession = getCookie('user');
   const router = useRouter();
+  const [scroll, setScroll] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => setScroll(window.scrollY > 50);
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   if (loading) return null;
 
   return (
-    <header>
+    <header
+      className={`transition-all duration-300 ${scroll ? 'opacity-50 py-1' : 'opacity-100 py-4'}`}
+    >
       <h1>
         <Link href="/" className="animate-text-change">
           {'{REST}'}
         </Link>
       </h1>
+
       <nav>
+        <LanguageSwitcher />
         {user || userSession ? (
           <NavBtn
             href="/"
-            text="Sign Out"
+            text={t('signOut')}
             onClick={() => {
               signOut(auth);
               deleteCookie('user');
@@ -35,8 +52,8 @@ export default function Header() {
           />
         ) : (
           <>
-            <NavBtn href="/sign-in" text="Sign In" />
-            <NavBtn href="/sign-up" text="Sign Up" />
+            <NavBtn href="/sign-in" text={t('signIn')} />
+            <NavBtn href="/sign-up" text={t('signUp')} />
           </>
         )}
       </nav>
--- app/components/main-route/Welcome/Welcome.tsx
+++ app/components/main-route/Welcome/Welcome.tsx
@@ -4,28 +4,34 @@ import NavBtn from '../NavBtn/NavBtn';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/app/firebase/config';
 import { getCookie } from 'cookies-next';
+import { useTranslations } from 'next-intl';
 
 export default function Welcome() {
+  const t = useTranslations('Welcome');
+  const tHeader = useTranslations('Header');
+
   const [user, loading] = useAuthState(auth);
   const userSession = getCookie('user');
+
   if (loading) return null;
+
   return (
     <section>
       {user || userSession ? (
         <>
-          <h2>Welcome Back, {user?.displayName || 'User'}!</h2>
+          <h2>{t('welcomeBack', { name: user?.displayName || t('user') })}</h2>
           <nav>
-            <NavBtn href="/RESTful" text="Rest Client" />
-            <NavBtn href="/history" text="History" />
-            <NavBtn href="/variables" text="Variables" />
+            <NavBtn href="/RESTful" text={t('restClient')} />
+            <NavBtn href="/history" text={t('history')} />
+            <NavBtn href="/variables" text={t('variables')} />
           </nav>
         </>
       ) : (
         <>
-          <h2>Welcome!</h2>
+          <h2>{t('welcome')}</h2>
           <nav>
-            <NavBtn href="/sign-in" text="Sign In" />
-            <NavBtn href="/sign-up" text="Sign Up" />
+            <NavBtn href="/sign-in" text={tHeader('signIn')} />
+            <NavBtn href="/sign-up" text={tHeader('signUp')} />
           </nav>
         </>
       )}
