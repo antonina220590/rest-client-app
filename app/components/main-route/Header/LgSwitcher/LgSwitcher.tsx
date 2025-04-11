672636e00bf036c3ac2db294be96d0f38acd923d
antonina220590 <tonya2290@gmail.com>
2025-04-11 15:39:42 +0300

    fix: update routing under language change

--- app/components/main-route/Header/LgSwitcher/LgSwitcher.tsx
+++ app/components/main-route/Header/LgSwitcher/LgSwitcher.tsx
@@ -1,28 +1,38 @@
 'use client';
 
 import { useRouter, usePathname } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useTransition } from 'react';
 
 export default function LanguageSwitcher() {
   const router = useRouter();
   const pathname = usePathname();
   const [selectedLanguage, setSelectedLanguage] = useState('en');
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
-    const locale = pathname.split('/')[1];
+    const pathSegments = window.location.pathname.split('/');
+    const locale = pathSegments[1];
     if (locale === 'ru' || locale === 'en') {
       setSelectedLanguage(locale);
     }
-  }, [pathname]);
+  }, []);
 
   const handleLanguageChange = (language: string) => {
     setSelectedLanguage(language);
-    router.push(`/${language}${pathname.substring(3)}`);
+    startTransition(() => {
+      const currentPathWithoutLocale = pathname.startsWith(
+        '/' + selectedLanguage
+      )
+        ? pathname.substring(3)
+        : pathname;
+      router.replace('/' + language + currentPathWithoutLocale);
+    });
   };
 
   return (
     <div className="language-switcher">
       <select
+        disabled={isPending}
         value={selectedLanguage}
         onChange={(e) => handleLanguageChange(e.target.value)}
       >
