4084b5a8c2f87eb49ba04c94154ec2fddfd5c4aa
antonina220590 <tonya2290@gmail.com>
2025-04-16 18:12:22 +0300

    fix: change link to rest client page from history page

--- app/components/history/HistoryList.tsx
+++ app/components/history/HistoryList.tsx
@@ -69,7 +69,7 @@ export default function HistoryList() {
           {t('emptyTitle')}
         </h2>
         <p className="mb-6 text-muted-foreground">{t('emptyDescription')}</p>
-        <Link href="/rest-client">
+        <Link href="/GET">
           <button className="btn-primary w-full md:w-auto text-sm md:text-base px-3 py-2 md:px-4 md:py-2">
             {t('goToClientButton')}
           </button>
