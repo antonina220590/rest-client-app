bba00bcab4d011affc6f40c11de1c28e92cefec3
gbogdanova <gaaalo44ka@gmail.com>
2025-04-16 09:23:50 -0700

    fix: protect sign-in/up pages

--- middleware.ts
+++ middleware.ts
@@ -21,6 +21,10 @@ export function middleware(req: NextRequest) {
   const regex =
     /^\/([a-zA-Z-]{2,3})?(\/(history|GET|POST|PUT|DELETE|PATCH|OPTIONS|HEAD))(\/.*)?$/;
 
+  if (userSession && /\/(sign-in|sign-up)/.test(pathname)) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
   if (!userSession && restrictedPaths.some((_path) => regex.test(pathname))) {
     return NextResponse.redirect(new URL('/', req.url));
   }
