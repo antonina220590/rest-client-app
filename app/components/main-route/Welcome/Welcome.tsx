ad0de97d70cf7e159d1780ff44a04cb14ad9715d
gbogdanova <gaaalo44ka@gmail.com>
2025-04-19 21:52:27 -0700

    feat: add user session

--- app/components/main-route/Welcome/Welcome.tsx
+++ app/components/main-route/Welcome/Welcome.tsx
@@ -5,19 +5,36 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/app/firebase/config';
 import { getCookie } from 'cookies-next';
 import { useTranslations } from 'next-intl';
+import { useState, useEffect } from 'react';
 
 export default function Welcome() {
   const t = useTranslations('Welcome');
   const tHeader = useTranslations('Header');
 
   const [user, loading] = useAuthState(auth);
-  const userSession = getCookie('user');
+  const [userSession, setUserSession] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkUserCookie = () => {
+      const userCookie = getCookie('user');
+      if (userCookie) {
+        setUserSession(true);
+      } else {
+        setUserSession(false);
+      }
+    };
+
+    checkUserCookie();
+    const intervalId = setInterval(checkUserCookie, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   if (loading) return null;
 
   return (
     <section>
-      {user || userSession ? (
+      {userSession ? (
         <>
           <h2>{t('welcomeBack', { name: user?.displayName || t('user') })}</h2>
           <nav>
